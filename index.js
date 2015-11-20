e960ff0a6088c97277b114417ea4d649246ae3c9
Artem Petrov <saint.tema@gmail.com>
2015-11-20 15:38:18 -0600

    Fixed indents

--- index.js
+++ index.js
@@ -5,22 +5,21 @@ var utils = require("loader-utils"),
 module.exports = function(content) {
 
 	var id = this.resource,
-		matches,
-		tpl;
+            matches,
+            tpl;
 
 	if (!id) {
-		throw new Error('File name is empty.');
+            throw new Error('File name is empty.');
 	}
 
 	matches = id.match(/([^/\?#]+).*$/);
 
 	if (matches === null) {
-		throw new Error('File name not found in "' + id + '"');
+            throw new Error('File name not found in "' + id + '"');
 	}
 
     id = matches.length ? matches[1] : id;
 	twig({ id: id, data: content }).compile({ module: 'node' });
 
 	return 'module.exports = ' + tpl.match(/(?:twig\()(.*)(?:\))/m)[1] + ';';
-
 }
