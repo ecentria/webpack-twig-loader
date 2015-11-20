6e697e33d542bbc62f278d487c4dbd9b4b2a797d
Artem Petrov <artem.petrov@OpticsPlanet.com>
2015-11-20 15:33:22 -0600

    changed template id to file name

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ var utils = require("loader-utils"),
 
 module.exports = function(content) {
 
-	var id = this.resource;
+	var id = this.resource,
+		matches,
+		tpl;
 
-	var tpl = twig({ id: id, data: content })
-		.compile({ module: 'node' });
+	if (!id) {
+		throw new Error('File name is empty.');
+	}
+
+	matches = id.match(/([^/\?#]+).*$/);
+
+	if (matches === null) {
+		throw new Error('File name not found in "' + id + '"');
+	}
+
+    id = matches.length ? matches[1] : id;
+	twig({ id: id, data: content }).compile({ module: 'node' });
 
 	return 'module.exports = ' + tpl.match(/(?:twig\()(.*)(?:\))/m)[1] + ';';
 
